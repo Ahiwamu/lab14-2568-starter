ebfa09a0379e53c286e164643b7381dc23033283
Ahiwamu <niceziio08@gmail.com>
2025-09-15 23:16:55 +0700

    finished

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -24,8 +24,8 @@ export default function MainLayout() {
       <AppShell.Footer>
         <FooterComponent
           year="2025"
-          fullName="chanadda"
-          studentId="670612039"
+          fullName="Weeraphat Boonju"
+          studentId="670610734"
         />
       </AppShell.Footer>
 
